434ef6414d8f08f95cf9d3d90fa3dc13af9cc2ac
Robert <27239435+lavavex@users.noreply.github.com>
2026-07-26 10:19:52 -0500

    Initial Commit

---
+++ FinanceWidget/FinanceWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceWidgetApp.swift
+//  FinanceWidget
+//
+//  Created by roberth on 7/26/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
